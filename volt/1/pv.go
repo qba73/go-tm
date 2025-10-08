bea2819f2a7c2c6ed5cb0501bb3ef96b7e28358d
Jakub Jarosz <jakub@jarosz.dev>
2025-10-08 21:30:25 +0100

    add error example
    Signed-off-by: Jakub Jarosz <jakub@jarosz.dev>

--- volt/1/pv.go
+++ volt/1/pv.go
@@ -1,1 +1,21 @@
 package volt
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrHighVoltage = errors.New("reached maximum safety voltage")
+	ErrPowerOutage = errors.New("cannot power on pumps")
+)
+
+func PowerOnPV(voltage int) error {
+	if voltage == 0 {
+		return ErrPowerOutage
+	}
+	if voltage > 250 {
+		return fmt.Errorf("switching the system: %w", ErrHighVoltage)
+	}
+	return nil
+}
--- volt/1/pv_test.go
+++ volt/1/pv_test.go
@@ -1,1 +1,18 @@
 package volt_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/qba73/volt"
+)
+
+func TestPowerOnPV_ReturnsPowerOutageErrorForLowVoltage(t *testing.T) {
+
+	err := volt.PowerOnPV(0)
+	want := volt.ErrPowerOutage
+
+	if !errors.Is(err, want) {
+		t.Errorf("want %, got %T", want, err)
+	}
+}
